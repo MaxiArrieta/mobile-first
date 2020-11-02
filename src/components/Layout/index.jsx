55699e7b9ceedab0aeb2116f509869bac70ab391
Maxi <arrieta.maxi18@gmail.com>
2020-11-02 17:08:58 -0300

    agregando seo

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,65 @@
 import React from "react"
 import Helmet from "react-helmet"
 import { GlobalStyle } from "../../styles"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Layout = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+  const metaDescription = site.siteMetadata.description
+  const defaultTitle = site.siteMetadata.title
   return (
     <>
-      <Helmet>
-        <title>Mobile First</title>
+      <Helmet
+        htmlAttributes={{ lang: "es" }}
+        title={defaultTitle}
+        titleTemplate={`${defaultTitle} | ${site.siteMetadata.author}`}
+        meta={[
+          {
+            name: `description`,
+            content: metaDescription,
+          },
+          {
+            property: `og:title`,
+            content: defaultTitle,
+          },
+          {
+            property: `og:description`,
+            content: metaDescription,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:creator`,
+            content: site.siteMetadata.author,
+          },
+          {
+            name: `twitter:title`,
+            content: defaultTitle,
+          },
+          {
+            name: `twitter:description`,
+            content: metaDescription,
+          },
+        ].concat()}
+      >
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Inter:wght@300;500&display=swap"
           rel="stylesheet"
